1ef21b4e1b8bd533ed4227ce89e13654ba4b477a
mikeku1116 <mikekuforgit@gmail.com>
2022-05-26 00:38:01 +0800

    5.7批次爬取多分頁的網頁資料

--- scraper.py
+++ scraper.py
@@ -8,38 +8,43 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get(
-    'https://shopee.tw/mall/%E5%B1%85%E5%AE%B6%E7%94%9F%E6%B4%BB-cat.11040925')
-time.sleep(5)
+for page in range(1, 3):
 
-ActionChains(driver).move_by_offset(100, 100).click().perform()
+    driver.get(
+        f'https://shopee.tw/mall/%E5%B1%85%E5%AE%B6%E7%94%9F%E6%B4%BB-cat.11040925/popular?pageNumber={page}')
+    time.sleep(5)
 
-cards = driver.find_elements(
-    By.CSS_SELECTOR, "div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
+    ActionChains(driver).move_by_offset(100, 100).click().perform()
 
-items = []
-for card in cards:
-    # ActionChains(driver).move_to_element(card).perform()
+    cards = driver.find_elements(
+        By.CSS_SELECTOR, "div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
 
-    title = card.find_element(
-        By.CSS_SELECTOR, "div[class='ie3A+n bM+7UW Cve6sh']").text
-    price = card.find_element(
-        By.CSS_SELECTOR, "div[class='vioxXd rVLWG6']").text
-    link = card.find_element(By.TAG_NAME, "a").get_attribute('href')
-    items.append((title, price, link))
+    items = []
+    for card in cards:
+        # ActionChains(driver).move_to_element(card).perform()
 
-# print(items)
+        title = card.find_element(
+            By.CSS_SELECTOR, "div[class='ie3A+n bM+7UW Cve6sh']").text
+        price = card.find_element(
+            By.CSS_SELECTOR, "div[class='vioxXd rVLWG6']").text
+        link = card.find_element(By.TAG_NAME, "a").get_attribute('href')
+        items.append((title, price, link))
 
-result = []
-for item in items:
-    driver.get(item[2])
+    # print(items)
 
-    for i in range(5):
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        time.sleep(3)
+    result = []
+    for item in items:
+        driver.get(item[2])
 
-    comments = driver.find_elements(By.CSS_SELECTOR, "div[class='Em3Qhp']")
-    for comment in comments:
-        result.append((item[0], item[1], comment.text))
-    break
-print(result)
+        for i in range(5):
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight)")
+            time.sleep(3)
+
+        comments = driver.find_elements(By.CSS_SELECTOR, "div[class='Em3Qhp']")
+        for comment in comments:
+            result.append((item[0], item[1], comment.text))
+        break
+
+    print(f"第{page}頁")
+    print(result)
