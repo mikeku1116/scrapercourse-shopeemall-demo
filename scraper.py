281a7990c8ee59e6674c320a274554fc27d4caa9
mikeku1116 <mikekuforgit@gmail.com>
2022-05-26 23:35:26 +0800

    5.8 3個常見的動態網頁等待機制

--- scraper.py
+++ scraper.py
@@ -4,6 +4,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -12,9 +14,16 @@ for page in range(1, 3):
 
     driver.get(
         f'https://shopee.tw/mall/%E5%B1%85%E5%AE%B6%E7%94%9F%E6%B4%BB-cat.11040925/popular?pageNumber={page}')
-    time.sleep(5)
 
     ActionChains(driver).move_by_offset(100, 100).click().perform()
+
+    locator = (By.CSS_SELECTOR,
+               "div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(locator),
+        "找不到指定的元素"
+    )
 
     cards = driver.find_elements(
         By.CSS_SELECTOR, "div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
@@ -39,7 +48,13 @@ for page in range(1, 3):
         for i in range(5):
             driver.execute_script(
                 "window.scrollTo(0, document.body.scrollHeight)")
-            time.sleep(3)
+
+        sub_locator = (By.CSS_SELECTOR, "div[class='Em3Qhp']")
+
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(sub_locator),
+            "找不到指定的元素"
+        )
 
         comments = driver.find_elements(By.CSS_SELECTOR, "div[class='Em3Qhp']")
         for comment in comments:
